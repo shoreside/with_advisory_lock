e333314365d9119e62cd32ff5c643b305b0c8827
Christian Schmidt <christian@bookmetender.com>
2018-10-24 16:10:24 +0200

    minimize loading

--- lib/with_advisory_lock.rb
+++ lib/with_advisory_lock.rb
@@ -6,12 +6,6 @@ module WithAdvisoryLock
   autoload :Concern
   autoload :Base
   autoload :DatabaseAdapterSupport
-  autoload :Flock
   autoload :MySQL, 'with_advisory_lock/mysql'
   autoload :NestedAdvisoryLockError
-  autoload :PostgreSQL, 'with_advisory_lock/postgresql'
-end
-
-ActiveSupport.on_load :active_record do
-  ActiveRecord::Base.send :include, WithAdvisoryLock::Concern
 end
